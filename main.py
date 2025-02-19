dfc2185ed726d08847ab79b08e35217e8561d946
PC <denzaz19@highgateschool.org.uk>
2025-02-19 11:57:39 +0100

    I added projectiles, made the enemies spawn every 30 frames and added health to the player, however there is a bug that i dont know how to fix. (Denys)

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ from pygame.locals import (
 # change to whatever 50x50 pixelart png you want with a transparent background
 player_img = "fish.png"
 enemy_img = "shark.png"
+attack_img= "bubble.png"
 
 # classes for the player and enemies and stuff will go here
 class Player(pygame.sprite.Sprite):
@@ -26,8 +27,11 @@ class Player(pygame.sprite.Sprite):
         self.img = pygame.image.load(player_img).convert_alpha()
         # can change to smth other than 50x50 pixels if you want
         self.rect = pygame.Rect(0, 0, 50, 50)
+        self.health = 5
+        self.invulnerable = False
+        self.invuln_timer = 0
 
-    # it moves
+        # it moves
     def move(self, pressed_keys):
         if pressed_keys[K_UP]:
             self.rect.move_ip(0, -v)
@@ -49,6 +53,18 @@ class Player(pygame.sprite.Sprite):
         if self.rect.bottom >= SCREEN_HEIGHT:
             self.rect.bottom = SCREEN_HEIGHT
 
+    def enemy_collision(self, enemies):
+        if self.invulnerable:
+            self.invuln_timer -= 1
+            if self.invuln_timer <= 0:
+                self.invulnerable = False
+        else:
+            for enemy in enemies:
+                if self.rect.colliderect(enemy.rect):
+                    self.health -= 1
+                    self.invulnerable = True
+                    self.invuln_timer = FPS  # 1 second of invulnerability
+                    break
 
 
 # Enemy class that chases the player
@@ -71,6 +87,36 @@ class Enemy(pygame.sprite.Sprite):
         dx, dy = dx / distance, dy / distance
         self.rect.move_ip(dx * self.speed, dy * self.speed)
 
+#maybe we should add a "tough enemy" class that will be identical to the enemy but just with more hp
+# class ToughtEnemy(pygame.sprite.Sprite):
+# pass
+
+
+class Projectile(pygame.sprite.Sprite):
+    def __init__(self, shooter):
+        super().__init__()
+        self.img = pygame.image.load(attack_img).convert_alpha()
+        self.rect = pygame.Rect(shooter.rect.right, shooter.rect.centery, 10, 10)
+        self.speed = 7
+
+    def move(self):
+        self.rect.move_ip(self.speed, 0)
+
+    def wall_collision(self):
+        if self.rect.left < 0:
+            self.kill()
+        if self.rect.right > SCREEN_WIDTH:
+            self.kill()
+        if self.rect.top <= 0:
+            self.kill()
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.kill()
+
+    def enemy_collision(self, enemy):
+        if (self.rect.right >= enemy.rect.left and self.rect.left <= enemy.rect.right and self.rect.bottom >= enemy.rect.top and self.rect.top <= enemy.rect.bottom):
+            self.kill()
+            enemy.kill()
+
 
 # quite important for pygame to work
 pygame.init()
@@ -79,12 +125,19 @@ pygame.init()
 SCREEN_WIDTH = 500
 SCREEN_HEIGHT = 500
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+shooting=False
 
 # player velocity
 v = 5
 
 # instantiating player object
 player = Player()
+
+font=pygame.font.SysFont("Arial", 20)
+
+bubble = Projectile(player)
+enemies=[]
+bubbles=[]
 
 # creates a group of all sprites making it easier later to update them all
 all_sprites = pygame.sprite.Group()
@@ -93,6 +146,7 @@ all_sprites.add(player)
 # makes framerate constant across different machines
 clock = pygame.time.Clock()
 FPS = 30
+count=0
 
 # game loop
 running = True
@@ -107,6 +161,15 @@ while running:
             if event.key == K_ESCAPE:
                 running = False
 
+            if event.key == pygame.K_z:
+                bubble = Projectile(player)
+                bubbles.append(bubble)
+                all_sprites.add(bubble)
+
+    text = font.render(f'Health: {player.health}', True, (255, 255, 255), 0)
+    textRect = text.get_rect()
+    textRect.center = (SCREEN_WIDTH // 2, 20)
+
     # gets dict of all keys pressed for moving player
     pressed_keys = pygame.key.get_pressed()
 
@@ -114,7 +177,32 @@ while running:
     player.move(pressed_keys)
     player.wall_collide()
 
+    if count%FPS==0:
+        enemy=Enemy()
+        all_sprites.add(enemy)
+        enemies.append(enemy)
+
+    for enemy in enemies:
+        enemy.move_towards_player(player.rect.center)
+
+    #there's this weird bug that makes the bubbles disappear but they still kill the sharks for some reason
+    #also damage is taken even after the enemies are killed
+    #i dont have a single idea on how to fix it
+    for bubble in bubbles:
+        bubble.move()
+        bubble.wall_collision()
+        for enemy in enemies:
+            if bubble.enemy_collision(enemy):
+                bubbles.remove(bubble)
+                enemies.remove(enemy)
+                bubble.kill()
+                enemy.kill()
+                break
+
+    player.enemy_collision(enemies)
+
     screen.fill((0, 0, 0))
+    screen.blit(text, textRect)
 
     # draw sprites to screen surface
     for entity in all_sprites:
@@ -124,6 +212,10 @@ while running:
     pygame.display.flip()
     # wait a bit
     clock.tick(FPS)
+    count+=1
+
+    if player.health <= 0:
+        running = False
 
 # goodbye
 pygame.quit()
